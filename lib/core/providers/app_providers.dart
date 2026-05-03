e862678e8a6b7da7e79063643f4647ba32cb96ed
finan <boutlot@hotmail.fr>
2026-05-03 02:30:28 -0400

    chore: drop unused foundation import

--- lib/core/providers/app_providers.dart
+++ lib/core/providers/app_providers.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../services/alert_engine.dart';
